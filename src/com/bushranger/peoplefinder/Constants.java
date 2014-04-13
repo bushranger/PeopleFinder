bc9d7c0335f9099f4f0d50e849c79a5548ab055e
Sagar <sagar0327@gmail.com>
2014-04-13 15:46:21 +0530

    Update Constants.java

--- src/com/bushranger/peoplefinder/Constants.java
+++ src/com/bushranger/peoplefinder/Constants.java
@@ -1,6 +1,6 @@
 package com.bushranger.peoplefinder;
 public class Constants {
-	public static final String ACCESS_KEY_ID = "AKIAIM2I6E4VZMPRJCBA";
-	public static final String SECRET_KEY = "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";
+	public static final String ACCESS_KEY_ID = "*******";
+	public static final String SECRET_KEY = "*******";
 
 }
